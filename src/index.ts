e35009d8fe2d4d81f6a5f0a05b722ded1fd88cc5
ahmad <eng.ahmed.anwar@gmail.com>
2021-11-06 12:05:45 +1300

    fix linting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import express from 'express';
- 
-const app = express();
- 
+import { log } from 'console'
+import express from 'express'
+
+const app = express()
+
 app.get('/', (_, response: express.Response) => {
-  response.send('Hello world!');
-});
+  response.send('Hello world!')
+})
 
 const PORT = 3000
 app.listen(PORT, () => {
-  console.log('listening on port ', PORT);
-});
+  log('listening on port ', PORT)
+})
